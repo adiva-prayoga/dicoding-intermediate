0d9e1f023acab8a0820a4258fd0c9ada5bd5c785
Adiva <prayoga.adiva@gmail.com>
2023-12-17 15:08:17 +0800

    :rotating_light: Fix Eslint warnings

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 
 import NotFoundPage from "./NotFoundPage";
 import Button from "../components/Button";
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import { Component } from "react";
 
 import NoteList from "../components/NoteList";
 import SearchBar from "../components/SearchBar";
