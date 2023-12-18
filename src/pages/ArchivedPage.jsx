2313878310b9df358043a8bc23170aad3891da3b
Adiva <prayoga.adiva@gmail.com>
2023-12-19 04:17:59 +0800

    :recycle: Utilizing query parameters for ArchivedPage

--- src/pages/ArchivedPage.jsx
+++ src/pages/ArchivedPage.jsx
@@ -4,42 +4,56 @@ import NoteList from "../components/NoteList";
 import SearchBar from "../components/SearchBar";
 
 import PropTypes from "prop-types";
-import { getArchivedNotes } from "../utils/local-data";
+import { useSearchParams } from "react-router-dom";
+
+import { getArchivedNotes, searchArchivedNotes } from "../utils/local-data";
+
+function ArchivedPageWrapper() {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const search = searchParams.get("search");
+
+  const changeSearchParams = (keyword) => {
+    setSearchParams({ search: keyword });
+  };
+
+  return (
+    <ArchivedPage handleSearch={changeSearchParams} activeKeyword={search} />
+  );
+}
 
 class ArchivedPage extends Component {
   constructor(props) {
     super(props);
 
+    const { activeKeyword } = this.props;
     this.state = {
-      searchResult: "",
+      searchResult: searchArchivedNotes(activeKeyword),
       notes: getArchivedNotes(),
     };
+
+    this.handleSearch = this.handleSearch.bind(this);
   }
 
   handleSearch = (searchResult) => {
     this.setState({
-      searchResult,
+      searchResult: searchArchivedNotes(searchResult),
     });
+
+    this.props.handleSearch(searchResult);
   };
 
-  filterNotes = (notes, searchResult) => {
-    if (!searchResult) {
-      return notes;
-    } else {
-      return notes.filter((note) =>
-        note.title.toLowerCase().includes(searchResult.toLowerCase())
-      );
-    }
-  };
   render() {
-    const { notes, searchResult } = this.state;
+    const { searchResult } = this.state;
 
     return (
       <section className="notes-section">
         <div className="container">
           <h1 className="title">Archive</h1>
-          <SearchBar handleSearch={this.handleSearch} />
-          <NoteList notes={this.filterNotes(notes, searchResult)} />
+          <SearchBar
+            handleSearch={this.handleSearch}
+            defaultKeyword={this.props.activeKeyword}
+          />
+          <NoteList notes={searchResult} />
         </div>
       </section>
     );
@@ -47,8 +61,8 @@ class ArchivedPage extends Component {
 }
 
 ArchivedPage.propTypes = {
-  notes: PropTypes.array,
-  searchResult: PropTypes.string,
+  handleSearch: PropTypes.func,
+  activeKeyword: PropTypes.string,
 };
 
-export default ArchivedPage;
+export default ArchivedPageWrapper;
